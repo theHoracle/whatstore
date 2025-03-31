33d6a1b91b673faf493acd3e2c613230c6dd1336
theHoracle <princemjames2@gmail.com>
2025-03-31 20:44:01 +0100

    updated api interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,21 +1,28 @@
 import { auth } from '@clerk/nextjs/server';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
 
-
-const api = axios.create({
+const api = applyCaseMiddleware(axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+}));
 
 api.interceptors.request.use(async (config) => {
   const { getToken } = await auth();
   const token = await getToken();
 
-  console.log('token', token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
+
+// Add type support for API calls
+export const apiCall = {
+  get: <T>(url: string) => api.get<T, AxiosResponse<T>>(url),
+  post: <T>(url: string, data: any) => api.post<T, AxiosResponse<T>>(url, data),
+  put: <T>(url: string, data: any) => api.put<T, AxiosResponse<T>>(url, data),
+  delete: <T>(url: string) => api.delete<T, AxiosResponse<T>>(url),
+};
 
 export default api;
