b894c845d28fa878efc1e2bf2695cfed38bd48c1
theHoracle <princemjames2@gmail.com>
2025-04-16 16:32:17 +0100

    added auth to supabase

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,20 @@
+import { useAuth } from '@clerk/nextjs'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+const { getToken } = useAuth()
+const token = await getToken({ template: "supabase" })
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    global: {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+})
+
 
 export default supabase
